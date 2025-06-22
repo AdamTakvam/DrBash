ac9da361c69e6a271004a58858001866da514234
Adam Takvam <adam.takvam@gmail.com>
2025-06-22 00:11:57 -0500

    Added an 'ls -l' wrapper that fixes inconsistencies in the way ls interprets trailing slashes in paths.

---
+++ system.shd/ll.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source "${USERLIB:-$HOME/lib}/arrays.sh"
+
+declare -a passFlags=('-l')
+declare -a passPaths=()
+declare -i longList=0
+
+for p in "$@"; do
+  case "$p" in
+    -*)
+      passFlags+=("$p") ;;
+    *)
+      # If the last character in a given path is not a /
+      #   then append a -d flag so it will give us the directory properties.
+      # If it does end in a / don't do anything 
+      #   and it will display the properties of the contents of the directory.
+      # If there was no trailing / and the path points to a symlink,
+      #   resolve the symlink and display the properties of the target directory.
+      if [ "${p: -1}" != '/' ]; then
+        passFlags+=('-d')
+        if [[ -L "$p" ]]; then
+          passPaths+=("$(realpath "$p")")
+        else
+          passPaths+=("$p")
+        fi
+      else
+        passPaths+=("$p") 
+      fi ;;
+  esac
+done
+
+exe="ls $(SerializeArray passFlags) $(SerializeArray -q passPaths)"
+eval "$exe"
+
+
