e177d51ff5a092d0993cd754baf40cd593146dce
Adam Takvam <adam.takvam@gmail.com>
2025-07-04 05:27:32 -0500

    Added command to explicitly separate just how wild you want to be with code commits

--- git.shd/pushit.sh
+++ git.shd/pushit.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cMsg="${1:-Changed code}"
-git add . \
-  && git commit -m "$cMsg" \
-  && git push
+Commit() {
+  cMsg="${1:-Changed code}"
+  git commit -m "$cMsg" \
+    && git push
+}
+
+case "$1" in
+  -a)
+    git add .
+    Commit "$2" ;;
+  "" | *)
+    Commit "$1" ;;
+esac
+
---
+++ git.shd/pushitall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pushit -a "$1"
