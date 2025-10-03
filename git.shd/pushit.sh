723458c0cabd3cb21dd20ebac39a06a7fee03328
Adam Takvam <adam.takvam@gmail.com>
2025-10-03 05:41:26 -0500

    Too many changes. I need to not wait so long between checkins...

--- git.shd/pushit.sh
+++ git.shd/pushit.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+Help() {
+  echo "Commits pending local code changes then pushes those changes to 'main'"
+  echo
+  echo "Usage: $(basename "$0") [-a|-h] [msg]"
+  echo
+  echo "Parameters:"
+  echo -e "\t-a\tAdd all untracked changes before committing."
+  echo -e "\t-h\tPrint this help screen."
+  echo -e "\tmsg\tYour commit message [default: Something you probably don't want]"
+  echo
+}
+
 Commit() {
-  cMsg="${1:-Changed code}"
-  git commit -m "$cMsg" \
-    && git push
+  cMsg="${1:-I have modified the code. Pray that I don\'t modify it further!}"
+  git commit -m "$cMsg" && git push
 }
 
 case "$1" in
+  -h | --help)
+    Help ;;
   -a)
     git add .
     Commit "$2" ;;
--- git.shd/pushitall.sh
+++ git.shd/pushitall.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-pushit -a "$1"
+Help() {
+  echo "Adds all untracked changes, then commits pending local code changes, then pushes those changes to 'main'"
+  echo "Equivalent to: pushit -a"
+  echo
+  echo "Usage: $(basename "$0") [-h] [msg]"
+  echo
+  echo "Parameters:"
+  echo -e "\t-h\tPrint this help screen."
+  echo -e "\tmsg\tYour commit message [default: Something you probably don't want]"
+  echo
+}
+
+case "${1-}" in
+  -h | --help)
+    Help ;;
+  "" | *)
+    pushit -a "$1" ;;
+esac
