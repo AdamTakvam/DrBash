3a0cde95a13dbc3967861652b029c5c4079e720a
Adam Takvam <adam.takvam@gmail.com>
2025-10-14 11:21:58 -0500

    Merged net-nm-utils.sh into net.sh

--- lib/net-nm-utils.sh
+++
@@ -1,5 +0,0 @@
-# Attempts to bring up the specified connection
-getItUp() {
-  [ "$1" ] && sudo nmcli conn up "$1"
-}
-
--- lib/net.sh
+++ lib/net.sh
@@ -1,15 +1,25 @@
+[[ -n $__net ]] && return 0
+declare -g __net=1
+
 # Populates specified array with the names of all configured network interfaces
 #   or prints them to stdout if no array specified.
 # + $1 = (opt) The name of the array the will hold the interface names
 # - stdout = If no array name passed in, the interface names. One per line.
-getInterfaces () {
-  [ "$1" ] && declare -n interfaces="$1" || declare -a interfaces=()
+GetInterfaces () {
+  [ "$1" ] && local -n interfaces="$1" || local -a interfaces=()
 
   interfaces+=($(ip addr | grep '^[1-9]' |  awk '{ print $2 }' | sed 's/://'))
 
-  if [ -z "$1" ]; then
-    for i in "${interfaces[@]}"; do
-      echo "$i"
+  if [[ -z "$1" ]]; then
+    for int in "${interfaces[@]}"; do
+      printf '%s\n' "$int"
     done
+    unset int
   fi
+}
+
+# Attempts to bring up the specified connection
+# + $1 = A NetworkManager connection ID
+RaiseConnection() {
+  [[ -n "$1" ]] && sudo nmcli conn up "$1"
 }
